58248e5c5e9360b1bb089b8c599f395ab8658854
AntonVaschenko322 <antonvaschenko6@gmail.com>
2025-05-27 17:14:30 +0300

    files

---
+++ tests/acc_test.cpp
@@ -0,0 +1,70 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "Account.h"
+#include "Transaction.h"
+
+class mockAccount : public Account
+{
+public:
+ mockAccount(int id, int balance):Account(id, balance){};
+ MOCK_METHOD(int, GetBalance, (), (const, override));
+ MOCK_METHOD(void, ChangeBalance, (int diff), (override));
+ using Account::is_locked_;
+ bool IsLocked() const { return is_locked_; }
+};
+
+class mockTransaction : public Transaction
+{
+ public:
+ MOCK_METHOD(void, SaveToDataBase, (Account& from, Account& to, int sum), (override)); 
+};
+
+TEST(trantest, SaveToDataBase)
+{
+
+ mockAccount acc1(1, 100);
+ mockAccount acc2(2, 873);
+ mockTransaction trans;
+ EXPECT_CALL(trans, SaveToDataBase(testing::_, testing::_, testing::_)).Times(1);	 
+ trans.SaveToDataBase(acc1, acc2, 150);
+}
+
+ 
+TEST(acctest, GetBalance)
+{
+ mockAccount Ben(1092, 357);
+ EXPECT_CALL(Ben,  GetBalance()).WillOnce(testing::Return(357)); 
+ int f = Ben.GetBalance();
+ EXPECT_EQ(f, 357);
+}
+
+TEST(acctest, ChangeBalance)
+{
+ mockAccount Ben(1092, 357);
+ EXPECT_CALL(Ben,  GetBalance()).WillOnce(testing::Return(357+2018));
+ EXPECT_CALL(Ben,  ChangeBalance(2018)).WillOnce(testing::Return());  
+ Ben.Lock();
+ Ben.ChangeBalance(2018);
+ EXPECT_EQ(Ben.GetBalance(), 2018+357);
+}
+TEST(acctest, Lock)
+{
+ mockAccount Ben(1092, 357);
+ Ben.Lock();
+ EXPECT_TRUE(Ben.IsLocked());
+}
+
+TEST(acctest, unlock)
+{
+ mockAccount Ben(1092, 357);
+ Ben.Unlock();
+ EXPECT_EQ(Ben.is_locked_, false);
+}
+
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    testing::InitGoogleMock(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
