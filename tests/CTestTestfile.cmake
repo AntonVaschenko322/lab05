33b952f5631ce647caa2a9d253008af824ecdb0a
AntonVaschenko322 <antonvaschenko6@gmail.com>
2025-05-27 19:30:06 +0300

    fixed

--- tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/antonvaschenko/projects/lab05/tests
-# Build directory: /home/antonvaschenko/projects/lab05/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
