fd747e2de01d2f8202f67487f9ca598c7bc8fb31
AntonVaschenko322 <antonvaschenko6@gmail.com>
2025-05-27 22:46:39 +0300

    Update acc_test.cpp

--- tests/acc_test.cpp
+++ tests/acc_test.cpp
@@ -1,28 +1,18 @@
 #include <string>
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
-//#include "googletest/googletest/include/gtest/gtest.h"
-//#include "googletest/googlemock/include/gmock/gmock.h"
 #include "Transaction.h"
 #include "Account.h"
 using ::testing::_;
 
 class MockTransaction : public Transaction {
-//class MockTransaction {
 public:
-	//MockTransaction() : Transaction() {};
-	//MOCK_METHOD(bool, Make, (Account& from, Account& to, int sum), ());
-	//MOCK_METHOD(void, Credit, (Account& accout, int sum), ());
-	//MOCK_METHOD(bool, Debit, (Account& accout, int sum), ());
-	//MOCK_METHOD(int, fee, (), (const));
-	//MOCK_METHOD(void, set_fee, (int fee));
 	MOCK_METHOD(void, SaveToDataBase, (Account& from, Account& to, int sum), (override));
 };
 
 class MockAccount : public Account {
 public:
 	MockAccount(int id, int balance) : Account(id, balance) {};
-	//~MockAccount()
 	MOCK_METHOD(int, GetBalance, (), (const, override));
 	MOCK_METHOD(void, ChangeBalance, (int diff), (override));
 	MOCK_METHOD(void, Lock, (), (override));
@@ -38,40 +28,25 @@ TEST(Transaction_test, test_set_fee) {
 
 TEST(MockTransaction_test, test_SaveToDataBase)
 {
-	/*MockAccount acc1(1, 100);
-	MockAccount acc2(2, 873);*/
 	Account acc1(1, 200);
 	Account acc2(2, 873);
-	//::testing::NiceMock<MockTransaction> trans;
 	MockTransaction trans;
-	//EXPECT_TRUE(Acc.GetBalance() == 999);
-	//EXPECT_CALL(acc1, GetBalance()).Times(1);
-	// 
-	//bool succes = trans.Make(acc1, acc2, 150);
-	//EXPECT_TRUE(succes);
-
-	//EXPECT_CALL(trans, Make(_, _, _)).Times(1);
-	// 
-	EXPECT_CALL(trans, SaveToDataBase(_, _, _)).Times(1);
-
-	//EXPECT_CALL(acc1, GetBalance()).Times(3);
-	//Transaction trans;
-	//EXPECT_CALL(acc1, Lock()).Times(testing::AnyNumber());
-	/*EXPECT_CALL(trans, Make(_, _, _)).Times(1);
-	EXPECT_CALL(trans, Credit(_, _)).Times(1);*/
-
-	//bool succes = trans.Make(acc1, acc2, 150);
-	//EXPECT_TRUE(succes);
-	 
+	EXPECT_CALL(trans, SaveToDataBase(_, _, _)).Times(1);	 
 	trans.SaveToDataBase(acc1, acc2, 150);
-
-	//EXPECT_CALL(trans, fee).Times(1);
-
-	//int comis = trans.fee();
 	std::cout << acc1.id() << "\nÃÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÉÄÀ\n";
 	std::cout << acc1.GetBalance() << "\nÃÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÉÄÀ\n";
 	std::cout << acc2.id() << "\nÃÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÉÄÀ\n";
 	std::cout << acc2.GetBalance() << "\nÃÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÎÉÄÀ\n";
+}
+
+TEST(Transaction_test, test_Make) 
+{
+	Account acc1(1, 200);
+	Account acc2(2, 873);
+	Transaction trans;
+	bool succes = trans.Make(acc1, acc2, 150);
+	std::cout << trans.fee();
+	EXPECT_TRUE(acc1.GetBalance() == (50 - trans.fee()));
 }
 
 TEST(Account_test, test_GetBalance1)
