bcc8c2bc979786c832da59952e9d1f911efd9829
AntonVaschenko322 <antonvaschenko6@gmail.com>
2025-06-01 20:11:28 +0300

    Update acc_test.cpp

--- tests/acc_test.cpp
+++ tests/acc_test.cpp
@@ -14,8 +14,6 @@ public:
 class MockAccount : public Account {
 public:
 	MockAccount(int id, int balance) : Account(id, balance) {};
-	MOCK_METHOD(int, GetBalance, (), (const, override));
-	MOCK_METHOD(void, ChangeBalance, (int diff), (override));
 	MOCK_METHOD(void, Lock, (), (override));
 	MOCK_METHOD(void, Unlock, (), (override));
 	MOCK_METHOD(int, id, (), (const));
